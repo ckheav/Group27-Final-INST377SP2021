b8ce954fbfe38c0f19a6a6e309fb41d3cdc60ca4
Martin Noren <55893214+M-Noren@users.noreply.github.com>
2021-04-23 12:57:01 -0400

    Added route for SeaInfo table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -237,4 +237,90 @@ router.put('/regions', async (req, res) => {
   }
 });
 
+/// ///////////////////////////////////
+/// /////// SeaInfo  Endpoints ////////
+/// ///////////////////////////////////
+router.get('/sea_info', async (req, res) => {
+  try {
+    const events = await db.SeaInfo.findAll();
+    const reply = events.length > 0 ? { data: events } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.get('/sea_info/:sea_info_id', async (req, res) => {
+  try {
+    const event = await db.SeaInfo.findAll({
+      where: {
+        sea_info_id: req.params.sea_info_id,
+      }
+    });
+
+    res.json(event);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.post('/sea_info', async (req, res) => {
+  const events = await db.SeaInfo.findAll();
+  try {
+    const newEvent = await db.SeaInfo.create({
+      sea_info_id: req.body.sea_info_id,
+      full_tide_present: req.full_tide_present,
+      high_tide_rise_height: req.body.high_tide_rise_height,
+      high_tide_set_height: req.body.high_tide_set_height,
+      low_tide_rise_height: req.body.low_tide_rise_height,
+      weather_secondary_id: req.body.weather_secondary_id,
+      low_tide_set_height: req.body.low_tide_set_height,
+    });
+    res.json(newEvent);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.delete('/sea_info/:sea_info_id', async (req, res) => {
+  try {
+    await db.SeaInfo.destroy({
+      where: {
+        sea_info_id: req.params.sea_info_id,
+      }
+    });
+    res.send('Successfully Deleted');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.put('/sea_info', async (req, res) => {
+  try {
+    await db.SeaInfo.update(
+      {
+        full_tide_present: req.full_tide_present,
+        high_tide_rise_height: req.body.high_tide_rise_height,
+        high_tide_set_height: req.body.high_tide_set_height,
+        low_tide_rise_height: req.body.low_tide_rise_height,
+        weather_secondary_id: req.body.weather_secondary_id,
+        low_tide_set_height: req.body.low_tide_set_height,
+      },
+      {
+        where: {
+          sea_info_id: req.body.sea_info_id,
+        }
+      }
+    );
+    res.send('Successfully Updated');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
 export default router;
