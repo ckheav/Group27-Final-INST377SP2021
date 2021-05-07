735dfd07768990932e01df5fda0f9c562ef6c5e5
ckheav@terpmail.umd.edu <52798523+ckheav@users.noreply.github.com>
2021-05-06 21:14:49 -0400

    New entry js

---
+++ public/add_new_entry.js
@@ -0,0 +1,36 @@
+async function getData(){
+    const request = await fetch('/api/regions');
+    const json = await request.json();
+
+    return json.data;
+}
+async function recordSubmission(){
+    const form = document.querySelector('#recordSubmit');
+    const city = document.querySelector('#cityName');
+
+    form.addEventListener('submit', async(event) => {
+        event.preventDefault();
+        console.info('submitted form', event.target);
+        
+        const post = await fetch('/api/regions',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ city: name.value})
+        });
+    });
+}
+
+async function windowActions(){
+    console.log('loaded window');
+    const regions = await getData();
+    console.table(regions);
+
+    regions.forEach(hall =>{
+        const button = document.createElement('button');
+        button.innerText = 
+    });
+}
+
+window.onload = windowActions;
